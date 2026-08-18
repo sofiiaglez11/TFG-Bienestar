bc5a9e9ea87822af0db14cabad089284d71a4fce
sofiaglez <sofia.g2co@gmail.com>
2026-08-18 18:54:00 +0200

    Añadido  archivo docker-compose.dev.yml para desarrollo

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  // Permite acceder a los recursos de desarrollo desde otros dispositivos en la red local
+  allowedDevOrigins: ["192.168.0.144"],
 };
 
 export default nextConfig;
